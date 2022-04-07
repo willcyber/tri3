d9bc3f29839c061083bf325c88f3b21e6a7c8aa2
JL1080 <jamesleesoccer@gmail.com>
2022-04-07 12:43:27 -0700

    added to the init and main.py to add login manager

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from flask import render_template
 from __init__ import app
+from cruddy.app_crud import app_crud
+from cruddy.app_crud_api import app_crud_api
+
+app.register_blueprint(app_crud)
+app.register_blueprint(app_crud_api)
 
 
 
