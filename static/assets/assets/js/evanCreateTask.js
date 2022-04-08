2ba1d1fd0891dc29fdb32009e48cb42b90478748
baconlolz <evanjoelsanchez@gmail.com>
2022-04-07 22:43:53 -0700

    Create task

--- static/assets/assets/js/evanCreateTask.js
+++ static/assets/assets/js/evanCreateTask.js
@@ -1,28 +1,62 @@
-function plantWheat() {
-    document.getElementById("wheatButton").className = "noDisplayTd"
-    document.getElementById("wheatTime").className = ""
-    var timeleft = 10;
-    var downloadTimer = setInterval(function(){
-        timeleft--;
-        document.getElementById("wheatTime").innerText = timeleft;
-        if(timeleft <= 0)
-            clearInterval(downloadTimer);
-            document.getElementById("wheatTime").disabled = false
+let wheatTime = 6;
+let cornTime = 10;
+let carrotTime = 20;
+let tomatoTime = 30;
 
-    },1000);
+function plantWheat() {
+    let downloadTimer = setInterval(function(){
+        if(wheatTime <= 0){
+            clearInterval(downloadTimer);
+            document.getElementById("wheatButton").innerHTML = "Plant Wheat";
+            cropList[0] += 2
+            document.getElementById("wheatCount").innerHTML = "Wheat: " + cropList[0]
+        } else {
+            document.getElementById("wheatButton").innerHTML = wheatTime + " seconds remaining";
+        }
+        wheatTime -= 1;
+    }, 1000);
 }
 
 function plantCorn(){
-    alert('corn')
-}
+    let downloadTimer = setInterval(function(){
+        if(cornTime <= 0){
+            clearInterval(downloadTimer);
+            document.getElementById("cornButton").innerHTML = "Plant Corn";
+            cropList[1] += 2
+            document.getElementById("cornCount").innerHTML = "Corn: " + cropList[1]
+        } else {
+            document.getElementById("cornButton").innerHTML = cornTime + " seconds remaining";
+        }
+        cornTime -= 1;
+    }, 1000);}
 
 function plantCarrots(){
-    alert('carrot')
-}
+    let downloadTimer = setInterval(function(){
+        if(carrotTime <= 0){
+            clearInterval(downloadTimer);
+            document.getElementById("carrotButton").innerHTML = "Plant Carrots";
+            cropList[2] += 2
+            document.getElementById("carrotCount").innerHTML = "Carrots: " + cropList[2]
+        } else {
+            document.getElementById("carrotButton").innerHTML = carrotTime + " seconds remaining";
+        }
+        carrotTime -= 1;
+    }, 1000);}
 
 function plantTomatoes(){
-    alert('tomato')
-}
+    let downloadTimer = setInterval(function(){
+        if(tomatoTime <= 0){
+            clearInterval(downloadTimer);
+            document.getElementById("tomatoButton").innerHTML = "Plant Tomatoes";
+            cropList[3] += 2
+            document.getElementById("tomatoCount").innerHTML = "Tomatoes: " + cropList[3]
+        } else {
+            document.getElementById("tomatoButton").innerHTML = tomatoTime + " seconds remaining";
 
+        }
+        tomatoTime -= 1;
+    }, 1000);}
+
+cropList = [0,0,0,0]
 
 
