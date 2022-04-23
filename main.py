a9500dab328d51da96858788877c8d5544619474
JL1080 <jamesleesoccer@gmail.com>
2022-04-23 15:49:37 -0700

    adding planner to PBL project

--- main.py
+++ main.py
@@ -6,12 +6,6 @@ from cruddy.app_crud_api import app_crud_api
 app.register_blueprint(app_crud)
 app.register_blueprint(app_crud_api)
 
-from cruddy.app_crud import app_crud
-from cruddy.app_crud_api import app_crud_api
-
-app.register_blueprint(app_crud)
-app.register_blueprint(app_crud_api)
-
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -27,6 +21,10 @@ def alexCreateTask():
 @app.route('/timer')
 def timer():
     return render_template("timer.html")
+
+@app.route('/regularplanner')
+def regularplanner():
+    return render_template("regularplanner.html")
 
 @app.errorhandler(404)
 def page_not_found(e):
