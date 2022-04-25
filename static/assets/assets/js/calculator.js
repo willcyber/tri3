7d4eaa5115c5020e8f05839a66a66506d888861b
AlexDo <alexkwangmin@gmail.com>
2022-04-24 23:37:14 -0700

    Add calculator to PBL project

--- static/assets/assets/js/calculator.js
+++ static/assets/assets/js/calculator.js
@@ -2,24 +2,28 @@ class Calculator {
     constructor(previousOperandTextElement, currentOperandTextElement) {
         this.previousOperandTextElement = previousOperandTextElement
         this.currentOperandTextElement = currentOperandTextElement
-        this.clear()
+        this.clear() //reset the inputs
     }
 
+    //clear all different variables
     clear() {
         this.currentOperand = ''
         this.previousOperand = ''
         this.operation = undefined
     }
 
+    //clear a single number
     delete() {
         this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
 
+    //when user clicks on a number to add to the display
     appendNumber(number) {
         if (number === '.' && this.currentOperand.includes('.')) return
         this.currentOperand = this.currentOperand.toString() + number.toString()
     }
 
+    //when user clicks on any operation button
     chooseOperation(operation) {
         if (this.currentOperand === '') return
         if (this.previousOperand !== '') {
@@ -30,6 +34,7 @@ class Calculator {
         this.currentOperand = ''
     }
 
+    //takes values inside calculator and displays the result
     compute() {
         let computation
         const prev = parseFloat(this.previousOperand)
@@ -73,6 +78,7 @@ class Calculator {
         }
     }
 
+    //update the values inside of the output
     updateDisplay() {
         this.currentOperandTextElement.innerText =
             this.getDisplayNumber(this.currentOperand)
@@ -85,7 +91,7 @@ class Calculator {
     }
 }
 
-
+<!-- operation buttons works -->
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
@@ -96,6 +102,7 @@ const currentOperandTextElement = document.querySelector('[data-current-operand]
 
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
 
+//constantly update every time the user click on a button
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
         calculator.appendNumber(button.innerText)
